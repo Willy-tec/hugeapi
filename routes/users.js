7b063575d279c56dc373d2657c1e49c9b18d4533
william <wastiaux.william@gmail.com>
2022-11-12 07:00:47 +0100

    comprend pas

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,12 @@ const db = require("../services/db");
 router.get("/", (req, res, next) => {
   res.send(req.params);
 });
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   mailer(req.body.user, req.body.email);
   console.log(db);
   db.connect();
   let result;
-  db.query("SELECT * FROM users", (err, rows, fields) => {
+  await db.query("SELECT * FROM users", (err, rows, fields) => {
     if (err) throw err;
 
     console.log("The solution is: ", rows[0].email);
