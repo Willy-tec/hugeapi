6b322578feb6afef2dd6a3d048000e3ac5ca710f
william <wastiaux.william@gmail.com>
2022-11-12 07:12:37 +0100

    dfg

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let router = express.Router();
 let mailer = require("../services/mailer");
-const db = require("../services/db");
+// const db = require("../services/db");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -9,16 +9,16 @@ router.get("/", (req, res, next) => {
 });
 router.post("/", (req, res, next) => {
   mailer(req.body.user, req.body.email);
-  db.connect();
-  let result;
-  db.query("SELECT * FROM users", (err, rows, fields) => {
-    if (err) throw err;
+  // db.connect();
+  // let result;
+  // db.query("SELECT * FROM users", (err, rows, fields) => {
+  //   if (err) throw err;
 
-    console.log("The solution is: ", rows[0].email);
-    result = rows[0].email;
-  });
-  db.end();
-  console.log("The solution is: ", result);
+  //   console.log("The solution is: ", rows[0].email);
+  //   result = rows[0].email;
+  // });
+  // db.end();
+  // console.log("The solution is: ", result);
   res.send("result:" + result);
 });
 module.exports = router;
